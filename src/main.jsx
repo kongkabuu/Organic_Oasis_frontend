535352d93434a7add093f53555fe0e6409528e94
paul kabuu <kongkabuu9@gmail.com>
2023-07-27 16:06:45 +0300

    adde googleOAuth

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GoogleOAuthProvider clientId="30603288495-3aec4f7gv49rdc0a2l9plglrrgjf38vh.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
