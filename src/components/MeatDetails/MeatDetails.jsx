da882ef3b9aeeacfe4df739d806509d5480f6be7
Cassie <cassierasugu@gmail.com>
2023-07-28 17:16:22 +0300

    detailspage

--- src/components/MeatDetails/MeatDetails.jsx
+++ src/components/MeatDetails/MeatDetails.jsx
@@ -13,3 +13,31 @@ const MeatDetails = () => {
     const [loading, setLoading] = useState(false);
     const [meat, setMeat] = useState(null);
     const navigate = useNavigate();
+
+
+    useEffect(() => {
+        setLoading(true);
+        async function getMeatDetails(){
+          try{
+            const response = await fetch(`${URL}${id}.json`);
+            const data = await response.json();
+            console.log(data);
+
+            if(data){
+                const {description, title, covers, price} = data;
+                const newMeat = {
+                  description: description ? description.value : "No description found",
+                  title: title,
+                  cover_img: covers ? `https://..............${covers[0]}-L.jpg` : coverImg,
+                  price: price
+                };
+                setMeat(newMeat);
+            } else {
+              setMeat(null);
+            }
+            setLoading(false);
+          } catch(error){
+            console.log(error);
+            setLoading(false);
+          }
+        }
