a918dafe8ba834253e20ff704be6b03e287f32b5
sandra <nyamweasandra@gmail.com>
2023-07-31 13:17:07 +0300

    new logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+import Header from "./Components/Header";
+
 function App() {
-  return <>hello</>;
+  return (
+    <div>
+      <Header />
+      {/* Add other components or content here */}
+    </div>
+  );
 }
 
 export default App;
