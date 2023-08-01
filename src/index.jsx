84e4424b224dc6fcccfda2eca2a31d86eeb00be0
Cassie <cassierasugu@gmail.com>
2023-08-02 02:52:00 +0300

    just example

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "categories" element = {<Categories />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
+);
+
