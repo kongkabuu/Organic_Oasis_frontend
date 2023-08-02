00a176445c680efb8195fadf10f6e1035b981d1e
Cassie <cassierasugu@gmail.com>
2023-08-02 03:37:08 +0300

    Attempt

--- src/components/MeatList/Meat.jsx
+++ src/components/MeatList/Meat.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./MeatList.css";
+import './Meat.css';
 
-const Meat = (meat) => {
+const Meat = ({ title, price, cover_img }) => {
   return (
     <div className='meat-item flex flex-column flex-sb'>
       <div className='meat-item-img'>
-        <img src = {meat.cover_img} alt = "cover" />
+        <img src={cover_img} alt='cover' />
       </div>
       <div className='meat-item-info text-center'>
-        <Link to = {`/meat/${meat.id}`} {...meat}>
-          <div className='meat-item-info-item title fw-7 fs-18'>
-            <span>{meat.title}</span>
-          </div>
-        </Link>
-
+        <div className='meat-item-info-item title fw-7 fs-18'>
+          <span>{title}</span>
+        </div>
         <div className='meat-item-info-item price fs-15'>
           <span className='text-capitalize fw-7'>Total Price: </span>
-          <span>{meat.price}</span>
+          <span>{price}</span>
         </div>
-
-        </div>
+        <button className='add-to-cart-btn'>Add to Cart</button>
+      </div>
     </div>
- )
-}
+  );
+};
 
-export default Meat
+export default Meat;
--- src/components/MeatList/MeatList.jsx
+++ src/components/MeatList/MeatList.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
-import { useGlobalContext } from '../../context.';
-import Meat from "../MeatList/Meat";
-import Loading from "../Loader/Loader";
-import "./MeatList.css";
-
-
+import Meat from '../Meat/Meat'; // Update the import path
+import './MeatList.css';
 
 const MeatList = () => {
-  const {meat, loading, resultTitle} = useGlobalContext();
-  const meatWithCovers = meat.map((singleMeat) => {
-    return {
-      ...singleMeat,
-      // removing /works/ to get only id
-     // id: (singleMeat.id).replace("/works/", ""),
-     // cover_img: singleMeat.cover_id  `https://......../b/id/${singleMeat.cover_id}-L.jpg`
-    }
-  });
+  const meatData = [
+    {
+      id: 1,
+      title: 'Beef',
+      price: '$10.99',
+      cover_img: 'https://via.placeholder.com/180', // Placeholder image URL
+    },
+    {
+      id: 2,
+      title: 'Chicken',
+      price: '$8.99',
+      cover_img: 'https://i.pinimg.com/564x/fd/13/69/fd13698867e26a123d1bb5150436306f.jpg', // Placeholder image URL
+    },
+    {
+      id: 3,
+      title: 'Fish',
+      price: '$12.99',
+      cover_img: 'https://via.placeholder.com/180', // Placeholder image URL
+    },
+  ];
 
-  if(loading) return <Loading />;
   return (
     <section className='meatlist'>
       <div className='container'>
         <div className='section-title'>
-          <h2>{resultTitle}</h2>
+          <h2>Meat Products</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+            perspiciatis unde omnis iste natus error sit voluptatem.
+          </p>
         </div>
         <div className='meatlist-content grid'>
-          {
-            meatWithCovers.slice(0, 30).map((item, index) => {
-              return (
-                <Meat key = {index} {...item} />
-              )
-            })
-          }
+          {meatData.map((item) => (
+            <Meat key={item.id} {...item} />
+          ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MeatList
+export default MeatList;
