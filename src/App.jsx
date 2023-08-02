2cadb4ce32d1e9b5e269eabfd60aa5e515e853d0
Cassie <cassierasugu@gmail.com>
2023-08-02 11:57:55 +0300

    bevarage

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ function App () {
           <Route path="/fruits" component={FruitsList} />
           <Route path="/vegetables" component={VegetablesList} />
           <Route path="/spice" component={SpiceList} />
+          <Route path="/dairy" component={Dairy}/>
         </Switch>
         </Routes>
         </>
